7452b47c23e0900226c440a8e378d465692815d5
beatlesm <rj.geng@gmail.com>
2022-02-11 08:06:35 -0800

    Task 2: Endpoints - [PUT] /api/projects/:id

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -4,16 +4,16 @@ const md = require('./projects-middleware')
 const Project = require("./projects-model")
 
 router.get("/", (req, res, next) => {
-  Project.get()
-    .then(projects => {
-      res.status(200).json(projects)
-    })
-    .catch(next)
+    Project.get()
+        .then(projects => {
+        res.status(200).json(projects)
+        })
+        .catch(next)
 })
 
 router.get('/:id', md.validateProjectId, (req, res) => {
     res.json(req.project)  
-  });
+});
 
 router.post('/', md.validateProject, (req, res, next) => {
     
@@ -22,7 +22,15 @@ router.post('/', md.validateProject, (req, res, next) => {
         res.status(201).json(newProject)
       })
       .catch(next)
-      
 });  
+
+router.put('/:id', md.validateProjectId, md.validateProject, (req, res, next) => {
+    
+    Project.update(req.params.id, req.body)
+      .then(updatedProject => {      
+        res.status(201).json(updatedProject)
+      })
+      .catch(next)
+  });
 
 module.exports = router
